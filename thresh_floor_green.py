c6f6285d83279bfece10fd6f99e017b03a762af0
TINNP137 <54058706+TINNP137@users.noreply.github.com>
2022-08-08 15:16:13 +0700

    Update thresh_floor_green.py

--- thresh_floor_green.py
+++ thresh_floor_green.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import imutils
 
-img = cv2.imread('./data/video/cafetefria_ground.png')
+img = cv2.imread('./data/helpers/cafetefria_ground.png')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 lower_range = np.array([0,210,255]) # hmin smin vmin
@@ -21,44 +21,10 @@ cv2.imshow('Mask',mask)
 cv2.imshow('final',img_erode)
 cv2.imshow('final2',final_pic)
 
-# cv2.imwrite('./data/video/cafeteria_mask.png',img_erode)
+cv2.imwrite('./data/helpers/cafeteria_mask.png',img_erode)
 
 
 
 cv2.waitKey()
 
 cv2.destroyAllWindows()
-
-
-
-
-## if check contour
-# import cv2
-# import numpy as np
-# import imutils
-#
-# img = cv2.imread('./cafetefria_ground.png')
-# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#
-# lower_range = np.array([0,210,255]) # hmin smin vmin
-# upper_range = np.array([179,255,255]) # hmax smax vmax
-#
-# mask = cv2.inRange(hsv,lower_range,upper_range)
-#
-# #contours
-# cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# cnts = imutils.grab_contours(cnts)
-#
-#
-# # cnts1 = cv2.findContours(thresh1.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# # cnts1 = imutils.grab_contours(cnts1)
-#
-#
-# for c in cnts:
-#     cv2.drawContours(img,[c],-1,(0,0,255),2)
-#
-# cv2.imshow("image",img)
-# cv2.imshow('Mask',mask)
-# cv2.waitKey()
-#
-# cv2.destroyAllWindows()
